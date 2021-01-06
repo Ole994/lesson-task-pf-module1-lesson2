b6fd3da7839855f68e0bcf860799e310e3ae3daf
Ole994 <69842859+Ole994@users.noreply.github.com>
2021-01-06 17:02:04 +0100

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,40 @@
+//### Question 1
+
+var myAge= "27"
+
+
+
+
+
+
+
+//### Question 2
+//var dayOne = monday;
+
+//var dayTwo = tuesday;
+
+//var dayThree = wednesday;
+
+//var dayFour = thursday;
+
+//var dayFive = friday;
+
+//### Question 3
+//### Question 4
+//### Question 5
+var amount4
+console.log("theFrogsAreAngry")
+
+
+
+
+
+//### Question 6
+var time_Elapsed;
+var totalPrice;
+
+
+//### Question 7
+//### Question 8
+//### Question 9
+//### Question 10
